040c5a6546a44fb116d7626074a3948b5cdfcc44
bjsonghongxu <bjsonghongxu@corp.netease.com>
2021-07-23 15:52:14 +0800

    [init]初始化Java学习总结项目

---
+++ src/test/java/com/shx/learn/javas/JavasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shx.learn.javas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
